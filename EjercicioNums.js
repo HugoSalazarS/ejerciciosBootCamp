79cb5142a2e67c422097533a665ef27f50fe158b
HugoSalazarS <113810978+HugoSalazarS@users.noreply.github.com>
2022-10-29 20:50:27 -0500

    Add files via upload

---
+++ EjercicioNums.js
@@ -0,0 +1,12 @@
+// Ejercicio números
+
+altura = 180
+altmetr = 1.80
+peso = 79.00
+altrd = Math.ceil(altmetr)
+pesord = Math.floor(peso)
+max = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
+console.log(altrd);
+console.log(pesord);
+console.log(max);
