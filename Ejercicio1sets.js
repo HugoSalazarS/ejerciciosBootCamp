ccf48494232318f4a0f4fe6e62cd7e97aa337b5c
HugoSalazarS <113810978+HugoSalazarS@users.noreply.github.com>
2022-10-30 20:19:07 -0500

    Add files via upload

---
+++ Ejercicio1sets.js
@@ -0,0 +1,9 @@
+// Ejercicio 1 sets
+
+familia = new Set(["Valentina", "Maria Paz", "Hugo"]);
+console.log(familia);
+
+familia.add("Hugo");
+console.log(familia);
+familia.add("Javascript");
+console.log(familia);
